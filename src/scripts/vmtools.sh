e0839ea6278775f95aa9b65c1a200da0f2736ef0
keithchambers <chambers_keith@yahoo.com>
2014-09-22 07:52:41 -0700

    Format changes. Fixed exit to return 1.

--- src/scripts/vmtools.sh
+++ src/scripts/vmtools.sh
@@ -14,35 +14,31 @@ function remove-kernel-headers()
     rm -f rpms-before rpms-after
 }
 
-case ${PACKER_BUILDER_TYPE} in
-
-virtualbox-iso|virtualbox-ovf)
-    install-kernel-headers
-    VERSION=$(cat /home/vagrant/.vbox_version)
-    mount -o loop /home/vagrant/VBoxGuestAdditions_${VERSION}.iso /mnt
-    sh /mnt/VBoxLinuxAdditions.run
-    umount /mnt
-    ;;
-
-vmware-iso|vmware-vmx) 
-    yum install -y perl nfs-utils
-    install-kernel-headers
-    mkdir /mnt/vmfusion
-    mkdir /mnt/vmfusion-archive
-    mount -o loop /home/vagrant/linux.iso /mnt/vmfusion
-    tar xzf /mnt/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
-    /mnt/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
-    umount /mnt/vmfusion
-    rm -rf /mnt/vmfusion
-    rm -rf /mnt/vmfusion-archive
-    ;;
-
-*)
-    echo "Unknown Packer Builder Type ${PACKER_BUILDER_TYPE} selected."
-    echo "Known are virtualbox-iso|virtualbox-ovf|vmware-iso|vmware-ovf."
-    exit 0
-    ;;
-
+case "${PACKER_BUILDER_TYPE}" in
+    virtualbox-iso|virtualbox-ovf)
+        install-kernel-headers
+        VERSION=$(cat /home/vagrant/.vbox_version)
+        mount -o loop /home/vagrant/VBoxGuestAdditions_${VERSION}.iso /mnt
+        sh /mnt/VBoxLinuxAdditions.run
+        umount /mnt
+        ;;
+    vmware-iso|vmware-vmx) 
+        yum install -y perl nfs-utils
+        install-kernel-headers
+        mkdir /mnt/vmfusion
+        mkdir /mnt/vmfusion-archive
+        mount -o loop /home/vagrant/linux.iso /mnt/vmfusion
+        tar xzf /mnt/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
+        /mnt/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
+        umount /mnt/vmfusion
+        rm -rf /mnt/vmfusion
+        rm -rf /mnt/vmfusion-archive
+        ;;
+    *)
+        echo "Unknown Packer Builder Type ${PACKER_BUILDER_TYPE} selected."
+        echo "Known are virtualbox-iso|virtualbox-ovf|vmware-iso|vmware-ovf."
+        exit 1
+        ;;
 esac
 
 
