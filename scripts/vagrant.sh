e911cfba359710d296aa26fc0388a8c1d865dae9
keithchambers <chambers_keith@yahoo.com>
2016-11-25 20:57:57 -0800

    Nits.

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
 
-# configure ssh
-echo UseDNS no >> /etc/ssh/sshd_config
+SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YV\
+r+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6Iedplqo\
+PkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp\
+4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rR\
+gm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== \
+vagrant insecure public key"
 
 # setup vagrant user
 groupadd vagrant
@@ -10,11 +14,12 @@ echo "vagrant" | passwd --stdin vagrant
 echo "%vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
 sed -i 's/^.*requiretty/#Defaults requiretty/' /etc/sudoers
 
-# setup vagrant insecure public key
+# configure ssh
+echo UseDNS no >> /etc/ssh/sshd_config
+
+# add vagrant insecure public key
 mkdir /home/vagrant/.ssh
-cat > /home/vagrant/.ssh/authorized_keys << __PUB_KEY__
-ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
-__PUB_KEY__
+echo "${SSH_KEY}" > /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
 
