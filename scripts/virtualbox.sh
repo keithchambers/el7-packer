9002c73656a3bc8610bb989c8e0b867224363a67
keithchambers <chambers_keith@yahoo.com>
2016-11-25 20:46:41 -0800

    Switch to variables.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -x
 
 # install dependencies
-rpm -qa --qf "%{NAME}\n" > rpms-before
+RPMS_PRE=$(rpm -qa --qf "%{NAME}\n" | sort)
 yum install -y make bzip2 gcc kernel-devel kernel-headers
-rpm -qa --qf "%{NAME}\n" > rpms-after
 
 # install virtualbox guest additions
 VERSION=$(cat .vbox_version)
@@ -12,7 +11,8 @@ sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 
 # clean up
+RPMS_POST=$(rpm -qa --qf "%{NAME}\n" | sort)
+yum remove -y $(join -v 2 <(echo "${RPMS_PRE}") <(echo "${RPMS_POST}"))
 rm -f .vbox_version && rm -f "VBoxGuestAdditions_${VERSION}.iso"
-yum remove -y $(join -v 2 <(sort rpms-before) <(sort rpms-after))
 
 # EOF
